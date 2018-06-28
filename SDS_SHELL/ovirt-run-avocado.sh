ffce07148f3511621aca0663c25b9e0dd41f35d4
Jiang Yu <lnsyyj@hotmail.com>
2018-06-28 23:30:30 +0800

    Update ovirt-run-avocado.sh

--- SDS_SHELL/ovirt-run-avocado.sh
+++ SDS_SHELL/ovirt-run-avocado.sh
@@ -4,6 +4,7 @@ AVOCADO_NODE="192.168.100.219"
 
 function run_avocado() {
         ssh "root@${AVOCADO_NODE}" "pushd /root/avocado-cloudtest && git pull && python setup.py install && avocado run SDSMgmtRestAPITest && popd"
+        # ssh "root@${AVOCADO_NODE}" "pushd /root/avocado-cloudtest && git pull && git reset --hard f9ecadc0d4032b3b67fd889c2060df3b37a373b9 && python setup.py install && avocado run SDSMgmtRestAPITest && popd"
 }
 
 run_avocado
