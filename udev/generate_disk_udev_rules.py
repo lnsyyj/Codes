894996156e6ace9638feb29fb0bd93d1adb3b9e8
JiangYu <lnsyyj@hotmail.com>
2020-10-30 10:46:02 +0800

    update
    Signed-off-by: JiangYu <lnsyyj@hotmail.com>

--- udev/generate_disk_udev_rules.py
+++ udev/generate_disk_udev_rules.py
@@ -10,8 +10,10 @@ from jinja2 import Template
 #template_udev_content = Template('KERNEL=="sd*",ACTION=="remove", SUBSYSTEM=="block", ENV{DEVTYPE}=="disk", ENV{ID_SERIAL}=="{{ VAR_ID_SERIAL }}", RUN+="/bin/sh -c \'/usr/bin/python /etc/udev/rules.d/udev_stop_osd.py $name 2>/var/log/ceph/ceph-remove-udev.log 1>&2\'"')
 
 template_udev_content = Template('KERNEL=="sd*",ACTION=="remove", SUBSYSTEM=="block", ENV{DEVTYPE}=="disk", ENV{ID_SERIAL}=="{{ VAR_ID_SERIAL }}", RUN+="/bin/sh -c \'systemctl stop ceph-osd@{{ VAR_OSD_ID }}.service 2>/var/log/ceph/ceph-remove-udev.log 1>&2\'"')
+#template_udev_content_add = Template('KERNEL=="sd*",ACTION=="add", SUBSYSTEM=="block", ENV{DEVTYPE}=="disk", ENV{ID_SERIAL}=="{{ VAR_ID_SERIAL }}", RUN+="/bin/sh -c \'sleep 60;systemctl reset-failed;systemctl start ceph-osd@{{ VAR_OSD_ID }}.service;systemctl reset-failed;systemctl start ceph-osd@{{ VAR_OSD_ID }}.services;\'"')
 
 template_udev_file_name_prefix = Template('80-ceph-osd-remove-{{ VAR_DEVICES_NAME }}.rules')
+#template_udev_add_file_name_prefix = Template('80-ceph-osd-add-{{ VAR_DEVICES_NAME }}.rules')
 
 def usage(filename):
   print('usage: python {} [-h | --help]'.format(filename))
@@ -39,6 +41,11 @@ def generate_disk_udev_rule():
       file_name = template_udev_file_name_prefix.render(VAR_DEVICES_NAME=devices_name)
       with open(file_name,'w') as fp:
             fp.write(content)
+
+#      content = template_udev_content_add.render(VAR_ID_SERIAL=result_cmd_udevadm.strip().replace("\n", ""), VAR_OSD_ID=osd_id)
+#      file_name = template_udev_add_file_name_prefix.render(VAR_DEVICES_NAME=devices_name)
+#      with open(file_name,'w') as fp:
+#            fp.write(content)
 
 def reload_udev_rule():
   cmd = "udevadm control --reload"
